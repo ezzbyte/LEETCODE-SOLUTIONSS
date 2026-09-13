de2cb381668edfa9a2b2217028d89a9c761b4883
Sayyed Yasar Ali <ya0558494@gmail.com>
2026-09-13 16:06:03 +0530

    Time: 4 ms (22.32%), Space: 64 MB (44.07%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let current = nums[0];
+    let maxSum = nums[0];
+    for(let i = 1; i < nums.length; i++){
+        current = Math.max(nums[i], current + nums[i]);
+        maxSum = Math.max(maxSum, current);
+    }
+    return maxSum;
+};
